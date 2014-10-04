c54fe3c88299a567f6c4e5fa5ccd33720dbd9b0d
Eric Pai <ericpai@berkeley.edu>
2014-10-04 16:36:02 -0700

    updated SoundOnTouch

--- test/Assets/SoundOnTouch.js
+++ test/Assets/SoundOnTouch.js
@@ -1,17 +1,53 @@
 ﻿#pragma strict
+var lastLoopStartTime = -1.0;
+var volumes = new Array();
 
-function OnCollisionEnter(){
+function OnCollisionEnter(collision : Collision){
 	// volume sensitivity on keypress
-	var vel = this.rigidbody.velocity;
-	var mag = vel.magnitude;
+//	var vel = this.rigidbody.velocity;
+//	var mag = vel.magnitude;
+//	
+//	var norm_mag = mag / 2; // needs fine tuning
+//	Debug.Log(norm_mag);
+	var speed = collision.relativeVelocity.magnitude;
 	
-	var norm_mag = mag / 2; // needs fine tuning
-	Debug.Log(norm_mag);
-	audio.PlayOneShot(audio.clip, norm_mag);
-	// while pressed, sustain note
+	var norm_speed = speed/30;
+	
+	if (norm_speed >= 1) {
+		norm_speed = 1;
+	}
+	
+	audio.Play();
+	audio.volume = norm_speed;
+	
+	
+//	volumes.Push(norm_speed);
+
 }
 
 function OnCollisionExit(){
 	// while pressed, sustain note
-	audio.Stop();
+	//FadeAudio(0);
+	var t = Time.time;
+	lastLoopStartTime = t;
+//	audio.volume = volumes.Shift();
+	FadeOut(t);
+	//audio.Stop();
 }
+
+// for fadein:  fadeType = 1
+// for fadeout: fadeType = 0
+function FadeOut(startTime) {
+	Debug.Log("start: " + audio.volume);
+	var decrement = audio.volume / 25;
+	for (var i=0; i < 25; i++) {
+		if (lastLoopStartTime != startTime) {
+			Debug.Log("llst: " + lastLoopStartTime + "; st: " + startTime); 
+			break;
+		}
+		audio.volume -= decrement;
+		Debug.Log("i is: " + i + ", vol is: " + audio.volume);
+		yield new WaitForSeconds(0.01);
+	}
+	Debug.Log("end: " + audio.volume);
+}	
