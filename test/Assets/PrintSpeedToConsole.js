737a5ba2fb1515dd818ba201cc11f72017bbc5bb
Tim Van Ness <tvanness@berkeley.edu>
2014-10-04 10:59:04 -0700

    Test project directory

---
+++ test/Assets/PrintSpeedToConsole.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+function OnCollisionEnter(){
+	
+	var vel = this.rigidbody.velocity;
+	Debug.Log(vel);
+}
---
+++ test/Assets/SoundOnTouch.js
@@ -0,0 +1,5 @@
+﻿#pragma strict
+
+function OnCollisionEnter(){
+	//audio.Play();
+}
